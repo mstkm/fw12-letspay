c77459c1c3d18c2188f187d4688550924552040f
mstkm <99125333+mstkm@users.noreply.github.com>
2022-12-23 21:29:16 +0700

    create landing page

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,11 +1,26 @@
 /** @type {import('tailwindcss').Config} */
+
 module.exports = {
   content: [
     "./pages/**/*.{js,ts,jsx,tsx}",
     "./components/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
-    extend: {},
+    extend: {
+      fontFamily: {
+        'primary': 'Nunito Sans'
+      },
+      colors: {
+        'primary': '#FF5F00',
+        'secondary': '#3A3D42'
+      },
+      borderColor: {
+        'primary': '#FF5F00'
+      },
+      backgroundColor: {
+        'primary': '#FF5F00'
+      }
+    }
   },
-  plugins: [],
+  plugins: [require("daisyui")],
 }
