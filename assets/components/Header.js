509e5b4e12e423b6f0c43e40e7bbf0e272e17a89
mstkm <99125333+mstkm@users.noreply.github.com>
2022-12-25 09:42:52 +0700

    change style Header

--- assets/components/Header.js
+++ assets/components/Header.js
@@ -4,25 +4,21 @@ import User from '../images/user.png'
 
 const Header = () => {
   return(
-    <section className="flex items-center bg-primary md:bg-white rounded-b-3xl drop-shadow-md font-primary px-5 md:px-32 py-8">
-    <div className="md:flex-1">
-      <h1 className="hidden md:block font-bold text-3xl text-primary">FazzPay</h1>
-    </div>
-    <div className="mr-3 bg-slate-300 p-2 rounded">
-      <Image src={User} className='w-8' alt='photo-profile'/>
-    </div>
-    <div className="hidden md:block">
-      <h5>Robert Chandler</h5>
-      <p>+62 8139 3877 7946</p>
-    </div>
-    <div className="flex-1 md:hidden">
-      <p className="text-sm text-slate-500">Balance</p>
-      <h5 className="font-bold text-lg text-white md:text-secondary">Rp120.000</h5>
-    </div>
-    <div className="ml-8">
-      <Bell className="text-white md:text-secondary"/>
-    </div>
-  </section>
+    <section className="hidden md:flex items-center bg-primary md:bg-white rounded-b-3xl drop-shadow-md font-primary px-5 md:px-32 py-8">
+      <div className="flex-1">
+        <h1 className="font-bold text-3xl text-primary">FazzPay</h1>
+      </div>
+      <div className="mr-3 bg-slate-300 p-2 rounded">
+        <Image src={User} className='w-8' alt='photo-profile'/>
+      </div>
+      <div>
+        <h5 className='font-bold'>Robert Chandler</h5>
+        <p>+62 8139 3877 7946</p>
+      </div>
+      <div className="ml-8">
+        <Bell className="text-white md:text-secondary"/>
+      </div>
+    </section>
   )
 }
 
